adbfcea929f5c5f5e45a1f6ec4272d4e16059e33
Hamzamirza9084 <hamzamirza9084@gmail.com>
2024-12-14 20:27:34 +0530

    6.0

--- index.js
+++ index.js
@@ -9,26 +9,26 @@ dotenv.config();
 
 const app = express();
 
-// Middleware
+
 app.use(express.json());
 app.use(cookieParser());
 
-// CORS Configuration
+
 const corsOptions = {
-    origin: ['http://localhost:5173', 'https://gregarious-fudge-8d4f5d.netlify.app'], // Allow both local and deployed frontend URLs
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed methods
-    credentials: true, // Allow cookies
+    origin: ['http://localhost:5173', 'https://gregarious-fudge-8d4f5d.netlify.app'], 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+    credentials: true, 
 };
 app.use(cors(corsOptions));
 
-// Routes
+
 app.use('/auth', UserRouter);
 
 app.get("/", (req, res) => {
     res.json("Hello");
 });
 
-// MongoDB Connection
+
 mongoose
     .connect(process.env.MONGO_URI || "your_default_connection_string", {
         useNewUrlParser: true,
@@ -37,7 +37,7 @@ mongoose
     .then(() => console.log("Connected to MongoDB"))
     .catch((err) => console.error("MongoDB connection error:", err));
 
-// Start Server
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
